2f448d207c0b0cffa2128c5e327d2824be829dc5
Felipe do Prado <felippe_praddo287@hotmail.com>
2023-01-18 22:48:36 -0300

    initial setting on home screen
    initial setup on homescreen and moved style to styHome

--- screens/home.js
+++ screens/home.js
@@ -1,23 +1,226 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
+import {Image, ScrollView, Text, View} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import StyHome from './styless/styHome';
+import Foundation from 'react-native-vector-icons/Foundation';
+const FlagEUA = require('../assets/FlagEUA.png');
 function HomeScreen({navigation}) {
   
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#000000',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text1:{
-      fontSize:15,
-      color: '#fff'
-    }
-  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text1}> HomeScreen </Text>
+    <View style={StyHome.container}>
+      <View style={StyHome.container1}>
+        <View style={StyHome.containerFlag}>
+         <Image
+          style={StyHome.imagFlag}
+          source={FlagEUA}
+          resizeMode= "stretch"/>
+        </View>
+        <View style={StyHome.containerFlag}>
+          <MaterialIcons name="local-fire-department" style={StyHome.icon2}/>
+          <Text style={StyHome.text3}>0</Text>
+        </View>
+        <View style={StyHome.containerFlag}>
+          <MaterialCommunityIcons name="cards-diamond" style={StyHome.icon3}/>
+          <Text style={StyHome.text4}>5630</Text>
+        </View>
+        <View style={StyHome.containerFlag}>
+          <AntDesign name="heart" style={StyHome.icon4}/>
+          <Text style={StyHome.text4}>5</Text>
+        </View>
+      </View>
+      <View style={StyHome.container2}/>
+      <ScrollView style={StyHome.scroll}>
+        <View style={StyHome.container3}>
+          <View style={StyHome.view1}>
+           <Text style={StyHome.text1}>Unidade 1</Text>
+           <Text style={StyHome.text2}>Faça  pedidos  em  um  café,</Text>
+           <Text style={StyHome.text2}>apresente-se</Text>
+          </View>
+          <View style={StyHome.view2}>
+           <MaterialCommunityIcons name="notebook-multiple" style={StyHome.icon1}/>
+          </View>
+        </View>
+        <View style={StyHome.container4}>
+          <View style={StyHome.containerFlag1}>
+           <AntDesign name="star" style={StyHome.icon6}/>
+          </View>
+          <View style={StyHome.containerFlag3}>
+           <FontAwesome name="lock" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag2}>
+            <View style={StyHome.containerFlag4}>
+             <MaterialCommunityIcons name="treasure-chest" style={StyHome.icon5}/>
+            </View>
+            <View style={StyHome.containerFlag9}>
+             <FontAwesome name="child" style={StyHome.icon8}/>
+            </View>
+          </View>
+          <View style={StyHome.containerFlag3}>
+           <MaterialIcons name="menu-book" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag7}>
+           <MaterialCommunityIcons name="trophy" style={StyHome.icon5}/>
+          </View>
+        </View>
+        <View style={StyHome.container5}>
+          <View  style={StyHome.view1}>
+           <Text style={StyHome.text1}>Unidade 2</Text>
+           <Text style={StyHome.text2}>Converse sobre viagem</Text>
+          </View>
+          <View style={StyHome.view2}>
+           <MaterialCommunityIcons name="notebook-multiple" style={StyHome.icon1}/>
+          </View>
+        </View>
+        <View style={StyHome.container6}>
+          <View style={StyHome.containerFlag8}>
+           <Foundation name="next" style={StyHome.icon6}/>
+          </View>
+          <View style={StyHome.containerFlag6}>
+           <FontAwesome name="lock" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag2}>
+            <View style={StyHome.containerFlag10}>
+             <FontAwesome name="child" style={StyHome.icon8}/>
+            </View>
+            <View style={StyHome.containerFlag5}>
+             <MaterialCommunityIcons name="treasure-chest" style={StyHome.icon5}/>
+            </View>
+          </View>
+          <View style={StyHome.containerFlag6}>
+           <MaterialCommunityIcons name="cards-diamond" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag7}>
+            <MaterialIcons name="menu-book" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag3}>
+            <FontAwesome name="lock" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag2}>
+            <View style={StyHome.containerFlag4}>
+             <FontAwesome name="lock" style={StyHome.icon5}/>
+            </View>
+            <View style={StyHome.containerFlag9}>
+             <FontAwesome name="child" style={StyHome.icon8}/>
+            </View>
+          </View>
+          <View style={StyHome.containerFlag3}>
+           <MaterialCommunityIcons name="treasure-chest" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag7}>
+           <MaterialIcons name="menu-book" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag7}>
+           <MaterialCommunityIcons name="trophy" style={StyHome.icon5}/>
+          </View>
+        </View>
+        <View style={StyHome.container3}>
+          <View style={StyHome.view1}>
+           <Text style={StyHome.text1}>Unidade 3</Text>
+           <Text style={StyHome.text2}>Peça em um restaurante,</Text>
+           <Text style={StyHome.text2}>compartilhe contato</Text>
+          </View>
+          <View style={StyHome.view2}>
+           <MaterialCommunityIcons name="notebook-multiple" style={StyHome.icon1}/>
+          </View>
+        </View>
+        <View style={StyHome.container6}>
+          <View style={StyHome.containerFlag11}>
+           <Foundation name="next" style={StyHome.icon6}/>
+          </View>
+          <View style={StyHome.containerFlag3}>
+           <FontAwesome name="lock" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag2}>
+            <View style={StyHome.containerFlag4}>
+             <MaterialCommunityIcons name="treasure-chest" style={StyHome.icon5}/>
+            </View>
+            <View style={StyHome.containerFlag10}>
+             <FontAwesome name="child" style={StyHome.icon8}/>
+            </View>
+          </View>
+          <View style={StyHome.containerFlag3}>
+           <MaterialCommunityIcons name="cards-diamond" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag7}>
+           <MaterialIcons name="menu-book" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag6}>
+           <FontAwesome name="lock" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag2}>
+            <View style={StyHome.containerFlag9}>
+             <FontAwesome name="child" style={StyHome.icon8}/>
+            </View>
+            <View style={StyHome.containerFlag4}>
+             <FontAwesome name="lock" style={StyHome.icon5}/>
+            </View>
+          </View>
+          <View style={StyHome.containerFlag6}>
+           <MaterialCommunityIcons name="treasure-chest" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag7}>
+           <MaterialIcons name="menu-book" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag7}>
+           <MaterialCommunityIcons name="trophy" style={StyHome.icon5}/>
+          </View>
+        </View>
+        <View style={StyHome.container5}>
+          <View  style={StyHome.view1}>
+           <Text style={StyHome.text1}>Unidade 4</Text>
+           <Text style={StyHome.text2}>Flae sobre a família</Text>
+          </View>
+          <View style={StyHome.view2}>
+           <MaterialCommunityIcons name="notebook-multiple" style={StyHome.icon1}/>
+          </View>
+        </View>
+        <View style={StyHome.container6}>
+          <View style={StyHome.containerFlag12}>
+           <Foundation name="next" style={StyHome.icon6}/>
+          </View>
+          <View style={StyHome.containerFlag6}>
+            <FontAwesome name="lock" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag2}>
+            <View style={StyHome.containerFlag10}>
+             <FontAwesome name="child" style={StyHome.icon8}/>
+            </View>
+            <View style={StyHome.containerFlag5}>
+             <MaterialCommunityIcons name="treasure-chest" style={StyHome.icon5}/>
+            </View>
+          </View>
+          <View style={StyHome.containerFlag6}>
+           <MaterialCommunityIcons name="cards-diamond" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag7}>
+           <MaterialIcons name="menu-book" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag3}>
+           <FontAwesome name="lock" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag2}>
+            <View style={StyHome.containerFlag4}>
+             <FontAwesome name="lock" style={StyHome.icon5}/>
+            </View>
+            <View style={StyHome.containerFlag9}>
+             <FontAwesome name="child" style={StyHome.icon8}/>
+            </View>
+          </View>
+          <View style={StyHome.containerFlag3}>
+           <MaterialCommunityIcons name="treasure-chest" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag7}>
+           <MaterialIcons name="menu-book" style={StyHome.icon5}/>
+          </View>
+          <View style={StyHome.containerFlag7}>
+           <MaterialCommunityIcons name="trophy" style={StyHome.icon5}/>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
---
+++ screens/styless/styHome.js
@@ -0,0 +1,259 @@
+import { StyleSheet } from "react-native";
+
+const StyHome = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  container1: {
+    backgroundColor: '#808080',
+    width: '100%',
+    height: 45,
+    flexDirection: 'row'
+  },
+  container2: {
+    backgroundColor: '#696969',
+    width: '100%',
+    height: 2,
+  },
+  container3: {
+    flexDirection: 'row',
+    backgroundColor: '#808080',
+    width: '100%',
+    height: 105,
+  },
+  container4: {
+    backgroundColor: '#696969',
+    width: '100%',
+    height: 500,
+  },
+  container5: {
+    backgroundColor: '#808080',
+    width: '100%',
+    height: 80,
+    flexDirection: 'row',
+
+  },
+  container6: {
+    backgroundColor: '#696969',
+    width: '100%',
+    height: 960,
+  },
+  scroll: {
+    backgroundColor: '#808080',
+    width: '100%',
+  },
+  text1: {
+    marginTop:10,
+    fontSize:25,
+    color: '#fff'
+  },
+  text2: {
+    fontSize: 20,
+    color: '#fff'
+  },
+  text3: {
+    marginLeft: 5,
+    marginTop: 7,
+    fontSize: 22,
+    color: '#c0c0c0'
+  },
+  text4: {
+    marginLeft: 5,
+    marginTop: 7,
+    fontSize: 22,
+    color: '#fff'
+  },
+  view1: {
+    height: '100%',
+    width: '80%',
+    marginLeft: 15,
+    flexDirection: 'column'
+  },
+  view2: {
+    alignSelf: 'center',
+    height: 45,
+    width: 45,
+    marginLeft: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor:'#696969',
+    borderRadius: 10,
+    borderWidth: 2,
+  },
+  icon1: {
+    color: '#ffffff',
+    fontSize: 25,
+    alignSelf: 'center',
+  },
+  icon2: {
+    color: '#c0c0c0',
+    fontSize: 25,
+    alignSelf: 'center',
+  },
+  icon3: {
+    color: '#6495ed',
+    fontSize: 25,
+    alignSelf: 'center',
+  },
+  icon4: {
+    color: '#ff0000',
+    fontSize: 25,
+    alignSelf: 'center',
+  },
+  icon5: {
+    color: '#c0c0c0',
+    fontSize: 40,
+    alignSelf: 'center',
+  },
+  icon6: {
+    color: '#fff',
+    fontSize: 40,
+    alignSelf: 'center',
+  },
+  icon8: {
+    color: '#c0c0c0',
+    fontSize: 80,
+    alignSelf: 'center',
+  },
+  containerFlag: {
+    height: '100%', 
+    width: '29%',
+    flexDirection: 'row'
+  },
+  containerFlag1: {
+    marginLeft:220,
+    marginTop: 15,
+    height: 80, 
+    width: 80,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#7cfc00',
+    borderColor:'#c0c0c0',
+    borderWidth: 2,
+    borderRadius: 50,
+  },
+  containerFlag3: {
+    marginLeft:170,
+    marginTop: 15,
+    height: 80, 
+    width: 80,
+    alignContent: 'center',
+    justifyContent: 'center',
+    borderColor:'#c0c0c0',
+    borderWidth: 2,
+    borderRadius: 50,
+  },
+  containerFlag4: {
+    marginLeft:120,
+    marginTop: 15,
+    height: 80, 
+    width: 80,
+    alignContent: 'center',
+    justifyContent: 'center',
+    borderColor:'#c0c0c0',
+    borderWidth: 2,
+    borderRadius: 50,
+  },
+  containerFlag5: {
+    marginLeft:90,
+    marginTop: 15,
+    height: 80, 
+    width: 80,
+    alignContent: 'center',
+    justifyContent: 'center',
+    borderColor:'#c0c0c0',
+    borderWidth: 2,
+    borderRadius: 50,
+  },
+  containerFlag6: {
+    marginLeft:250,
+    marginTop: 15,
+    height: 80, 
+    width: 80,
+    alignContent: 'center',
+    justifyContent: 'center',
+    borderColor:'#c0c0c0',
+    borderWidth: 2,
+    borderRadius: 50,
+  },
+  containerFlag7: {
+    marginLeft:220,
+    marginTop: 15,
+    height: 80, 
+    width: 80,
+    alignContent: 'center',
+    justifyContent: 'center',
+    borderColor:'#c0c0c0',
+    borderWidth: 2,
+    borderRadius: 50,
+  },
+  containerFlag8: {
+    marginLeft:220,
+    marginTop: 15,
+    height: 80, 
+    width: 80,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ff69b4',
+    borderColor:'#c0c0c0',
+    borderWidth: 2,
+    borderRadius: 50,
+  },
+  containerFlag9: {
+    marginLeft:90,
+    marginTop: 15,
+    height: 80, 
+    width: 80,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  containerFlag10: {
+    marginLeft:120,
+    marginTop: 15,
+    height: 80, 
+    width: 80,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  containerFlag11: {
+    marginLeft:220,
+    marginTop: 15,
+    height: 80, 
+    width: 80,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#008000',
+    borderColor:'#c0c0c0',
+    borderWidth: 2,
+    borderRadius: 50,
+  },
+  containerFlag12: {
+    marginLeft:220,
+    marginTop: 15,
+    height: 80, 
+    width: 80,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#32cd32',
+    borderColor:'#c0c0c0',
+    borderWidth: 2,
+    borderRadius: 50,
+  },
+   containerFlag2: {
+    alignContent: 'center',
+    flexDirection: 'row',
+   
+  },
+  imagFlag: {
+    marginLeft: 15,
+    alignSelf: 'center',
+    borderColor:'#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    height: 35,
+     width: 45
+  }
+});
+  export default StyHome
