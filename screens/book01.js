6f2c12a2b7cabf2c3d74c23fd4bbff746dbe0881
Felipe do Prado <felippe_praddo287@hotmail.com>
2023-01-22 16:41:57 -0300

    adjusted book01 screen layout
    adjusted book01 screen layout

--- screens/book01.js
+++ screens/book01.js
@@ -1,176 +1,267 @@
-import React from 'react';
-import {StyleSheet, Image, ScrollView, Text, View} from 'react-native';
+  import React from 'react';
+import { Image, ScrollView, Text, View} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-const CorujaLivro1 = require('../assets/Corujas-Livros/corujas-Livro1.png')
+import StyBook01 from './styless/styBook01';
+const CorujaLivro1 = require('../assets/books/book-01/corujas-Livro1.png')
+const Imagem01 = require('../assets/books/book-01/Imagem1-Livro1.jpg');
+const Imagem02 = require('../assets/books/book-01/Imagem2-Livro1.jpg');
+const Imagem03 = require('../assets/books/book-01/Imagem3-Livro1.jpg');
 
 function Book01Screen({navigation}) {
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      width: '100%',
-      height: '100%',
-    },
-    text1:{
-      marginLeft: 5,
-      marginTop: 10,
-      fontSize:25,
-      color: '#dcdcdc'
-    },
-    text2:{
-      alignSelf: 'center',
-      fontSize:30,
-      color: '#fff'
-    },
-    text3:{
-      marginTop: 5,
-      alignSelf: 'center',
-      fontSize:24,
-      color: '#dcdcdc'
-    },
-    text4:{
-      alignSelf: 'center',
-      fontSize:24,
-      color: '#dcdcdc'
-    },
-    text5:{
-      marginTop: 20,
-      marginLeft: 20,
-      fontSize:24,
-      color: '#87cefa'
-    },
-    text6:{
-      marginTop: 20,
-      marginLeft:20,
-      fontSize:24,
-      color: '#fff'
-    },
-    text7:{
-      marginTop: 10,
-      marginLeft:10,
-      fontSize:20,
-      color: '#fff'
-    },
-    text8:{
-      marginTop: 10,
-      marginLeft:10,
-      fontSize:20,
-      color: '#d3d3d3',
-    },
-    imagCoruja1: {
-      marginLeft: 15,
-      alignSelf: 'center',
-      height: 180,
-      width: 180
-    },
-    container2: {
-      backgroundColor: '#808080',
-      width: '100%',
-      height: 3,
-    },
-    container3: {
-      flexDirection: 'row',
-      marginTop: 20,
-      marginLeft: 20,
-      width: 200,
-      height: 100,
-      borderColor: '#d3d3d3',
-      borderRadius: 20,
-      borderWidth: 2,
-    },
-    container4: {
-      flexDirection: 'row',
-      marginLeft: 222,
-      width: 200,
-      height: 100,
-      borderColor: '#d3d3d3',
-      borderRadius: 20,
-      borderWidth: 2,
-    },
-    container5: {
-      flexDirection: 'row',
-      marginLeft: 20,
-      width: 170,
-      height: 100,
-      borderColor: '#d3d3d3',
-      borderRadius: 20,
-      borderWidth: 2,
-    },
-    container6: {
-      flexDirection: 'row',
-      marginTop: 20,
-      marginLeft: 20,
-      width: 170,
-      height: 100,
-      borderColor: '#d3d3d3',
-      borderRadius: 20,
-      borderWidth: 2,
-    },
-    scroll: {
-      backgroundColor: '#696969',
-      width: '100%',
-    },
-    icon1: {
-      color: '#87cefa',
-      fontSize: 30,
-      marginTop: 30,
-      marginLeft: 10,
-    },
-  });
+
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.scroll}>
+    <View style={StyBook01.container}>
+      <ScrollView style={StyBook01.scroll}>
         <View>
-          <Text style={styles.text1}> X </Text>
-          <Image style={styles.imagCoruja1} source={CorujaLivro1} resizeMode= "stretch"/>
-          <Text style={styles.text2}>Guia da Unidade 1</Text>
-          <Text style={styles.text3}>Explore explicações de gramática e</Text>
-          <Text style={styles.text4}>expressões importantes dessa</Text>
-          <Text style={styles.text4}>unidade.</Text>
-          <View style={styles.container2}/>
-          <Text style={styles.text5}>EXPRESSÔES IMPORTANTES</Text>
-          <Text style={styles.text6}>Faça pedido em um café</Text>
+          <Text style={StyBook01.text1}> X </Text>
+          <Image style={StyBook01.imagCoruja1} source={CorujaLivro1} resizeMode= "stretch"/>
+          <Text style={StyBook01.text2}>Guia da Unidade 1</Text>
+          <Text style={StyBook01.text3}>Explore explicações de gramática e</Text>
+          <Text style={StyBook01.text4}>expressões importantes dessa</Text>
+          <Text style={StyBook01.text4}>unidade.</Text>
+          <View style={StyBook01.container2}/>
+          <Text style={StyBook01.text5}>EXPRESSÔES IMPORTANTES</Text>
+          <Text style={StyBook01.text6}>Faça pedido em um café</Text>
         </View>
-        <View style={styles.container3}>
-          <AntDesign name="sound" style={styles.icon1}/>
+        <View style={StyBook01.container3}>
+          <AntDesign name="sound" style={StyBook01.icon1}/>
           <View>
-            <Text style={styles.text7}>Tea or coffee?</Text>
-            <Text style={styles.text8}>Chá ou café?</Text>
+            <Text style={StyBook01.text7}>Tea or coffee?</Text>
+            <Text style={StyBook01.text8}>Chá ou café?</Text>
           </View>
         </View>
-        <View style={styles.container4}>
-          <AntDesign name="sound" style={styles.icon1}/>
+        <View style={StyBook01.container4}>
+          <AntDesign name="sound" style={StyBook01.icon1}/>
           <View>
-            <Text style={styles.text7}>Tea, please.</Text>
-            <Text style={styles.text8}>Chá, por favor.</Text>
+            <Text style={StyBook01.text7}>Tea, please.</Text>
+            <Text style={StyBook01.text8}>Chá, por favor.</Text>
           </View>
         </View>
-        <View style={styles.container5}>
-          <AntDesign name="sound" style={styles.icon1}/>
+        <View style={StyBook01.container5}>
+          <AntDesign name="sound" style={StyBook01.icon1}/>
           <View>
-            <Text style={styles.text7}>With milk.</Text>
-            <Text style={styles.text8}>Com leite</Text>
+            <Text style={StyBook01.text7}>With milk.</Text>
+            <Text style={StyBook01.text8}>Com leite</Text>
           </View>
         </View>
-        <View style={styles.container6}>
-          <AntDesign name="sound" style={styles.icon1}/>
+        <View style={StyBook01.container6}>
+          <AntDesign name="sound" style={StyBook01.icon1}/>
           <View>
-            <Text style={styles.text7}>Thank you!</Text>
-            <Text style={styles.text8}>Obrigado!</Text>
+            <Text style={StyBook01.text7}>Thank you!</Text>
+            <Text style={StyBook01.text8}>Obrigado!</Text>
           </View>
         </View>
-        <View style={styles.container4}>
-          <AntDesign name="sound" style={styles.icon1}/>
+        <View style={StyBook01.container4}>
+          <AntDesign name="sound" style={StyBook01.icon1}/>
           <View>
-            <Text style={styles.text7}>Yu're welcome.</Text>
-            <Text style={styles.text8}>De nada.</Text>
+            <Text style={StyBook01.text7}>Yu're welcome.</Text>
+            <Text style={StyBook01.text8}>De nada.</Text>
           </View>
         </View>
-        <View style={styles.container5}>
-          <AntDesign name="sound" style={styles.icon1}/>
+        <View style={StyBook01.container5}>
+          <AntDesign name="sound" style={StyBook01.icon1}/>
           <View>
-            <Text style={styles.text7}>Goobye!</Text>
-            <Text style={styles.text8}>Adeus!</Text>
+            <Text style={StyBook01.text7}>Goobye!</Text>
+            <Text style={StyBook01.text8}>Adeus!</Text>
+          </View>
+        </View>
+        <View style={StyBook01.container8}>
+          <Text style={StyBook01.text5}>EXPLICAÇÃO</Text>
+          <Text style={StyBook01.text6}>Ser ou não ser?</Text>
+          <View style={StyBook01.container7}>
+            <Text style={StyBook01.text9}>Em português, nós dizemos</Text>
+            <Text style={StyBook01.text10}>eu fal</Text>
+            <Text style={StyBook01.text11}>o</Text>
+            <Text style={StyBook01.text12}>,</Text>
+            <Text style={StyBook01.text10}>voce fal</Text>
+            <Text style={StyBook01.text11}>a</Text>
+            <Text style={StyBook01.text12}>,</Text>
+          </View>
+          <View style={StyBook01.container7}>
+            <Text style={StyBook01.text13}>nós fal</Text>
+            <Text style={StyBook01.text15}>amos</Text>
+            <Text style={StyBook01.text17}>, etc., mas, em inglês, as</Text>
+          </View>
+          <View style={StyBook01.container7}>
+            <Text style={StyBook01.text16}>terminações dos</Text>
+            <View>
+              <Text style={StyBook01.text19}>verbos </Text>
+              <Text style={StyBook01.text14}>............</Text>
+            </View>
+            <Text style={StyBook01.text19}>não mudam tanto.</Text>
+          </View>
+          <View style={StyBook01.container7}>
+            <Text style={StyBook01.text9}>Uma exceção é o verbo</Text>
+            <View>
+              <Text style={StyBook01.text10}>to be</Text>
+              <Text style={StyBook01.text14}>.........</Text>
+            </View>
+            <Text style={StyBook01.text12}>, que signica</Text>
+          </View>
+          <View style={StyBook01.container7}>
+            <Text style={StyBook01.text16}>tanto</Text>
+            <Text style={StyBook01.text18}>ser</Text>
+            <Text style={StyBook01.text19}>como</Text>
+            <Text style={StyBook01.text18}>estar</Text>
+            <Text style={StyBook01.text20}>.</Text>
+          </View>
+          <View style={StyBook01.container9}>
+            <View style={StyBook01.container7}>
+              <View  style={StyBook01.container10}>
+                <Text style={StyBook01.text21}>sujeito</Text>
+                <Text style={StyBook01.text22}>............</Text>
+              </View>
+              <View style={StyBook01.container11}/>
+              <View  style={StyBook01.container10}>
+                <Text style={StyBook01.text21}>verbo (to be)</Text>
+                <Text style={StyBook01.text22}>.....................</Text>
+              </View>
+            </View>
+            <View style={StyBook01.container12}/>
+            <View style={StyBook01.container7}>
+              <View  style={StyBook01.container13}>
+                <Text style={StyBook01.text21}>I</Text>
+                <Text style={StyBook01.text22}>..</Text>
+                <Text style={StyBook01.text24}>eu</Text>
+              </View>
+              <View style={StyBook01.container11}/>
+              <View  style={StyBook01.container13}>
+                <Text style={StyBook01.text23}>am</Text>
+                <Text style={StyBook01.text22}>......</Text>
+                <Text style={StyBook01.text24}>sou / estou</Text>
+              </View>
+            </View>
+            <View style={StyBook01.container12}/>
+            <View style={StyBook01.container7}>
+              <View  style={StyBook01.container14}>
+                <Text style={StyBook01.text21}>you</Text>
+                <Text style={StyBook01.text22}>.......</Text>
+                <Text style={StyBook01.text24}>tu</Text>
+                <Text style={StyBook01.text24}>você</Text>
+              </View>
+              <View style={StyBook01.container11}/>
+              <View  style={StyBook01.container14}>
+                <Text style={StyBook01.text23}>are</Text>
+                <Text style={StyBook01.text22}>......</Text>
+                <Text style={StyBook01.text24}>és / estás</Text>
+                <Text style={StyBook01.text24}>é / está</Text>
+              </View>
+            </View>
+            <View style={StyBook01.container12}/>
+            <View style={StyBook01.container7}>
+              <View  style={StyBook01.container13}>
+                <Text style={StyBook01.text21}>he</Text>
+                <Text style={StyBook01.text22}>.....</Text>
+                <Text style={StyBook01.text24}>ele</Text>
+              </View>
+              <View style={StyBook01.container11}/>
+              <View  style={StyBook01.container13}>
+                <Text style={StyBook01.text23}>is</Text>
+                <Text style={StyBook01.text22}>....</Text>
+                <Text style={StyBook01.text24}>é / está</Text>
+              </View>
+            </View>
+            <View style={StyBook01.container12}/>
+            <View style={StyBook01.container7}>
+              <View  style={StyBook01.container13}>
+                <Text style={StyBook01.text21}>she</Text>
+                <Text style={StyBook01.text22}>.......</Text>
+                <Text style={StyBook01.text24}>ela</Text>
+              </View>
+              <View style={StyBook01.container15}/>
+              <View  style={StyBook01.container13}>
+                <Text style={StyBook01.text23}>is</Text>
+                <Text style={StyBook01.text22}>....</Text>
+                <Text style={StyBook01.text24}>é / está</Text>
+              </View>
+            </View>
+          </View>
+          <View>
+            <Text style={StyBook01.text25}>Mesmo que, em português, possamos dizer</Text>
+            <View style={StyBook01.container7}>
+              <Text style={StyBook01.text16}>tanto</Text>
+              <Text style={StyBook01.text18}>Eu sou um menimo</Text>
+              <Text style={StyBook01.text19}>como apenas</Text>
+              <Text style={StyBook01.text18}>Sou um</Text>
+            </View>
+            <View style={StyBook01.container7}>
+              <Text style={StyBook01.text13}>menino</Text>
+              <Text style={StyBook01.text17}>,</Text>
+              <Text style={StyBook01.text19}>em inglês, palavras como</Text>
+              <Text style={StyBook01.text18}>eu</Text>
+              <Text style={StyBook01.text19}>e</Text>
+              <Text style={StyBook01.text18}>você</Text>
+            </View>
+            <Text style={StyBook01.text16}>são sempre necessárias.</Text>
+          </View>
+          <Image style={StyBook01.imag01} source={Imagem01} resizeMode= "stretch"/>
+          <View style={StyBook01.container7}>
+            <AntDesign name="sound" style={StyBook01.icon2}/>
+            <Text style={StyBook01.text11}>I</Text>
+            <Text style={StyBook01.text10}>am a boy. </Text>
+            <Text style={StyBook01.text11}>You</Text>
+            <Text style={StyBook01.text10}>are a girl.</Text>
+          </View>
+          <View style={StyBook01.container7}>
+            <Text style={StyBook01.text28}>...................</Text>
+            <Text style={StyBook01.text14}>.......................</Text>
+          </View>
+          <View style={StyBook01.container7}>
+            <Text style={StyBook01.text26}>Eu</Text>
+            <Text style={StyBook01.text19}>sou um menino.</Text>
+            <Text style={StyBook01.text27}>Você</Text>
+            <Text style={StyBook01.text19}>é uma menina.</Text>
+          </View>
+          <View style={StyBook01.container16}/>
+          <View style={StyBook01.container17}>
+            <Text style={StyBook01.text5}>EXPLICAÇÃO</Text>
+            <Text style={StyBook01.text6}>Ah, não!</Text>
+            <View style={StyBook01.container7}>
+              <Text style={StyBook01.text9}>Para dizer</Text>
+              <Text style={StyBook01.text10}>eu </Text>
+              <Text style={StyBook01.text11}>nâo </Text>
+              <Text style={StyBook01.text12}>ou</Text>
+              <Text style={StyBook01.text10}>você </Text>
+              <Text style={StyBook01.text11}>não</Text>
+              <Text style={StyBook01.text10}>é </Text>
+              <Text style={StyBook01.text12}>em inglês,</Text>
+            </View>
+            <View style={StyBook01.container7}>
+              <Text style={StyBook01.text16}>adicione</Text>
+              <View>
+                <Text style={StyBook01.text15}> not </Text>
+                <Text style={StyBook01.text14}>.....</Text>
+              </View>
+              <Text style={StyBook01.text17}>depois do</Text>
+              <View>
+                <Text style={StyBook01.text17}> verbo!</Text>
+                <Text style={StyBook01.text14}>.........</Text>
+              </View>
+            </View>
+            <Image style={StyBook01.imag01} source={Imagem02} resizeMode= "stretch"/>
+            <View style={StyBook01.container7}>
+              <AntDesign name="sound" style={StyBook01.icon2}/>
+              <View>
+                <Text style={StyBook01.text10}>You are Stephen. </Text>
+                <Text style={StyBook01.text14}>...........................</Text>
+              </View>
+            </View>
+            <Text style={StyBook01.text26}>Você é o Stephen.</Text>
+            <Image style={StyBook01.imag01} source={Imagem03} resizeMode= "stretch"/>
+            <View style={StyBook01.container7}>
+              <AntDesign name="sound" style={StyBook01.icon2}/>
+              <Text style={StyBook01.text10}>You are </Text>
+              <Text style={StyBook01.text11}>not</Text>
+              <Text style={StyBook01.text10}>Stephen!</Text>
+            </View>
+            <View style={StyBook01.container7}>
+              <Text style={StyBook01.text28}>   .................................</Text>
+            </View>
+            <View style={StyBook01.container7}>
+              <Text style={StyBook01.text26}>Você</Text>
+              <Text style={StyBook01.text19}>não</Text>
+              <Text style={StyBook01.text27}>é o Stephen!</Text>
+            </View>
           </View>
         </View>
       </ScrollView>
---
+++ screens/styless/styBook01.js
@@ -0,0 +1,306 @@
+import { StyleSheet } from "react-native";
+
+const StyBook01 = StyleSheet.create({
+        container: {
+          flex: 1,
+          width: '100%',
+          height: '100%',
+        },
+        text1:{
+          marginLeft: 5,
+          marginTop: 10,
+          fontSize:25,
+          color: '#999'
+        },
+        text2:{
+          alignSelf: 'center',
+          fontSize:30,
+          color: '#ffffff'
+        },
+        text3:{
+          marginTop: 5,
+          alignSelf: 'center',
+          fontSize:24,
+          color: '#999'
+        },
+        text4:{
+          alignSelf: 'center',
+          fontSize:24,
+          color: '#999'
+        },
+        text5:{
+          marginTop: 20,
+          marginLeft: 20,
+          fontSize:18,
+          color: '#87cefa'
+        },
+        text6:{
+          marginTop: 25,
+          marginLeft:20,
+          fontSize:24,
+          color: '#ffffff'
+        },
+        text7:{
+          marginTop: 17,
+          marginLeft:10,
+          fontSize:20,
+          color: '#ffffff'
+        },
+        text8:{
+          marginTop: 10,
+          marginLeft:10,
+          fontSize:20,
+          color: '#999',
+        },
+        text9:{
+          marginTop: 25,
+          marginLeft:20,
+          fontSize:19,
+          color: '#999',
+        },
+        text10:{
+          marginLeft: 7,
+          marginTop: 25,
+          fontSize:19,
+          color: '#ffffff',
+        },
+        text11:{
+          marginTop: 25,
+          fontSize:19,
+          color: '#87cefa',
+        },
+        text12:{
+          marginTop: 25,
+          fontSize:19,
+          color: '#999',
+        },
+        text13:{
+          marginLeft: 20,
+          marginTop: 5,
+          fontSize:19,
+          color: '#ffffff',
+        },
+        text14:{
+          marginLeft: 6,
+          marginTop: -20,
+          fontSize: 19,
+          color: '#c0c0c0',
+        },
+        text15:{
+          marginTop: 5,
+          fontSize:19,
+          color: '#87cefa',
+        },
+        text16:{
+          marginTop: 5,
+          marginLeft:20,
+          fontSize:19,
+          color: '#999',
+        },
+        text17:{
+          marginTop: 5,
+          fontSize:19,
+          color: '#999',
+        },
+        text18:{
+          marginLeft: 5,
+          marginTop: 5,
+          fontSize:19,
+          color: '#fff',
+        },
+        text19:{
+          marginLeft: 5,
+          marginTop: 5,
+          fontSize:19,
+          color: '#999',
+        },
+        text20:{
+          marginTop: 0,
+          fontSize:19,
+          color: '#999',
+        },
+        text21:{
+          marginLeft: 20,
+          marginTop: 10,
+          fontSize:19,
+          color: '#ffffff',
+        },
+        text22:{
+          marginLeft: 18,
+          marginTop: -20,
+          fontSize: 19,
+          color: '#c0c0c0',
+        },
+        text23:{
+          marginLeft: 20,
+          marginTop: 10,
+          fontSize:19,
+          color: '#87cefa',
+        },
+        text24:{
+          marginLeft: 20,
+          marginTop: 10,
+          fontSize:19,
+          color: '#888',
+        },
+        text25:{
+          marginTop: 25,
+          marginLeft:20,
+          fontSize:19,
+          color: '#999',
+        },
+        text26:{
+          marginLeft: 20,
+          marginTop: 5,
+          fontSize:19,
+          color: '#666',
+        },
+        text27:{
+          marginLeft: 5,
+          marginTop: 5,
+          fontSize:19,
+          color: '#666',
+        },
+        text28:{
+          marginLeft: 125,
+          marginTop: -20,
+          fontSize: 19,
+          color: '#c0c0c0',
+        },
+    
+        imagCoruja1: {
+          marginLeft: 15,
+          alignSelf: 'center',
+          height: 180,
+          width: 180
+        },
+        imag01: { 
+          borderRadius: 15,
+          borderWidth: 4,
+          borderColor:'#87cefa',
+          marginTop: 30,
+          marginLeft: 10,
+          alignSelf: 'center',
+          height: 250,
+          width: 400,
+         
+        },
+        container2: {
+          marginTop: 10,
+          backgroundColor: '#808080',
+          width: '100%',
+          height: 3,
+        },
+        container3: {
+          flexDirection: 'row',
+          marginTop: 20,
+          marginLeft: 20,
+          width: 200,
+          height: 100,
+          borderColor: '#d3d3d3',
+          borderRadius: 20,
+          borderWidth: 2,
+        },
+        container4: {
+          flexDirection: 'row',
+          marginLeft: 222,
+          width: 200,
+          height: 100,
+          borderColor: '#d3d3d3',
+          borderRadius: 20,
+          borderWidth: 2,
+        },
+        container5: {
+          flexDirection: 'row',
+          marginLeft: 20,
+          width: 170,
+          height: 100,
+          borderColor: '#d3d3d3',
+          borderRadius: 20,
+          borderWidth: 2,
+        },
+        container6: {
+          flexDirection: 'row',
+          marginTop: 20,
+          marginLeft: 20,
+          width: 170,
+          height: 100,
+          borderColor: '#d3d3d3',
+          borderRadius: 20,
+          borderWidth: 2,
+        },
+        container7: {
+          flexDirection: 'row',
+        },
+        container8: {
+          backgroundColor: '#4444',
+          marginTop: 20,
+          width: '100%',
+          height: '100%',
+        },
+        container9: {
+          backgroundColor: '#444',
+          marginTop: 30,
+          marginLeft: 20,
+          width: '91%',
+          height: 467,
+          borderColor: '#87cefa',
+          borderRadius: 30,
+          borderWidth: 3,
+        },
+        container10: {
+          width: 190,
+          height: 50,
+        },
+        container11: {
+          backgroundColor: '#87cefa',
+          width: 3,
+          height: '100%',
+        },
+        container12: {
+          backgroundColor: '#87cefa',
+          width: '100%',
+          height: 3,
+        },
+        container13: {
+          width: 190,
+          height: 90,
+        },
+        container14: {
+          width: '49%',
+          height: 130,
+        },
+        container15: {
+          backgroundColor: '#87cefa',
+          width: 3,
+          height: 90,
+        },
+        container16: {
+          marginTop: 30,
+          backgroundColor: '#87cefa',
+          width: '100%',
+          height: 2,
+        },
+        container17:{
+          marginVertical: 20
+        },
+        scroll: {
+          backgroundColor: '#222',
+          width: '100%',
+          height: '100%',
+        },
+        icon1: {
+          color: '#87cefa',
+          fontSize: 30,
+          marginTop: 30,
+          marginLeft: 10,
+        },
+        icon2: {
+          color: '#87cefa',
+          fontSize: 30,
+          marginTop: 25,
+          marginLeft: 90,
+          marginHorizontal: 10,
+        },
+});
+  export default StyBook01
