a0cb2009d3cc5094817fb97f0a0e4c9ee19c5207
Felipe do Prado <felippe_praddo287@hotmail.com>
2023-01-24 19:02:08 -0300

    adjusted BooK04 screen layout
    adjusted BooK04 screen layout

--- screens/book04.js
+++ screens/book04.js
@@ -1,23 +1,188 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { Image, ScrollView, Text, View} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import StyBook04 from './styless/styBook04';
+const Imagem01 = require('../assets/books/book-04/Imagem1-Livro4.png');
 
 function Book04Screen({navigation}) {
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#000000',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text1:{
-      fontSize:15,
-      color: '#fff'
-    }
-  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text1}> Book04</Text>
+    <View style={StyBook04.container}>
+      <ScrollView style={StyBook04.scroll}>
+        <View>
+          <Text style={StyBook04.text1}> X </Text>
+          <Image style={StyBook04. imag01} source={Imagem01} resizeMode= "stretch"/>
+          <Text style={StyBook04.text2}>Guia da Unidade 4</Text>
+          <Text style={StyBook04.text3}>Explore explicações de gramática e</Text>
+          <Text style={StyBook04.text4}>expressões importantes dessa</Text>
+          <Text style={StyBook04.text4}>unidade.</Text>
+          <View style={StyBook04.container2}/>
+          <Text style={StyBook04.text5}>EXPRESSÔES IMPORTANTES</Text>
+          <Text style={StyBook04.text6}>Fale sobre a família</Text>
+        </View>
+        <View style={StyBook04.container3}>
+          <AntDesign name="sound" style={StyBook04.icon1}/>
+          <View>
+            <Text style={StyBook04.text7}>My family is small.</Text>
+            <Text style={StyBook04.text8}>Minha família é pequena.</Text>
+          </View>
+        </View>
+        <View style={StyBook04.container4}>
+          <AntDesign name="sound" style={StyBook04.icon1}/>
+          <View>
+            <Text style={StyBook04.text7}>I live with my parents and my brother.</Text>
+            <Text style={StyBook04.text8}>Eu mro com meus pais e meu irmão.</Text>
+          </View>
+        </View>
+        <View style={StyBook04.container5}>
+          <AntDesign name="sound" style={StyBook04.icon1}/>
+          <View>
+            <Text style={StyBook04.text7}>He has a cat and i have a dog.</Text>
+            <Text style={StyBook04.text8}>Ele tem um gato, e eu tenho um</Text>
+            <Text style={StyBook04.text8}>cachorro.</Text>
+          </View>
+        </View>
+        <View style={StyBook04.container6}>
+          <AntDesign name="sound" style={StyBook04.icon1}/>
+          <View>
+            <Text style={StyBook04.text7}>You have three brothers, right?</Text>
+            <Text style={StyBook04.text8}>Você tem Três irmãos, certo?</Text>
+          </View>
+        </View>
+        <View style={StyBook04.container16}>
+          <AntDesign name="sound" style={StyBook04.icon1}/>
+          <View>
+            <Text style={StyBook04.text7}>I have four sisters.</Text>
+            <Text style={StyBook04.text8}>Eu tenho quatro irmâs.</Text>
+          </View>
+        </View>
+        <View style={StyBook04.container8}>
+          <Text style={StyBook04.text5}>EXPLICAÇÃO</Text>
+          <Text style={StyBook04.text6}>You, you, and you</Text>
+          <View style={StyBook04.container7}>
+            <Text style={StyBook04.text9}>Em português, nós usamos</Text>
+            <Text style={StyBook04.text10}>tu</Text>
+            <Text style={StyBook04.text11}>,</Text>
+            <Text style={StyBook04.text10}>voce</Text>
+            <Text style={StyBook04.text11}>, e</Text>
+            <Text style={StyBook04.text10}>voces</Text>
+            <Text style={StyBook04.text11}>,</Text>
+          </View>
+          <Text style={StyBook04.text13}>mas, em inglês, para todas essas palavras você</Text>
+          <View style={StyBook04.container7}>
+            <Text style={StyBook04.text13}>só precisa usar</Text>
+            <View>
+              <Text style={StyBook04.text15}>you</Text>
+              <Text style={StyBook04.text12}>......</Text>
+            </View>
+            <Text style={StyBook04.text14}>.</Text>
+          </View>
+          <View style={StyBook04.container7}>
+            <Text style={StyBook04.text9}>Da mesma forma, para</Text>
+            <Text style={StyBook04.text10}>eles </Text>
+            <Text style={StyBook04.text11}>ou</Text>
+            <Text style={StyBook04.text10}>elas</Text>
+            <Text style={StyBook04.text11}>, você só</Text>
+          </View>
+          
+          <View style={StyBook04.container7}>
+            <Text style={StyBook04.text13}>precisa usar</Text>
+            <View>
+              <Text style={StyBook04.text15}>they</Text>
+              <Text style={StyBook04.text12}>......</Text>
+            </View>
+            <Text style={StyBook04.text14}>.</Text>
+          </View>
+          <View style={StyBook04.container9}>
+            <View style={StyBook04.container7}>
+              <View  style={StyBook04.container10}>
+                <Text style={StyBook04.text16}>sujeito</Text>
+                <Text style={StyBook04.text17}>............</Text>
+              </View>
+              <View style={StyBook04.container11}/>
+              <View  style={StyBook04.container10}>
+                <Text style={StyBook04.text16}>verbo (to have)</Text>
+                <Text style={StyBook04.text17}>.........  .............</Text>
+              </View>
+            </View>
+            <View style={StyBook04.container12}/>
+            <View style={StyBook04.container7}>
+              <View  style={StyBook04.container13}>
+                <Text style={StyBook04.text16}>I</Text>
+                <Text style={StyBook04.text17}>..</Text>
+                <Text style={StyBook04.text19}>eu</Text>
+              </View>
+              <View style={StyBook04.container11}/>
+              <View  style={StyBook04.container13}>
+                <Text style={StyBook04.text16}>have</Text>
+                <Text style={StyBook04.text17}>........</Text>
+                <Text style={StyBook04.text19}>tenho</Text>
+              </View>
+            </View>
+            <View style={StyBook04.container12}/>
+            <View style={StyBook04.container7}>
+              <View  style={StyBook04.container14}>
+                <Text style={StyBook04.text16}>you</Text>
+                <Text style={StyBook04.text17}>......</Text>
+                <Text style={StyBook04.text19}>tu</Text>
+                <Text style={StyBook04.text19}>você</Text>
+                <Text style={StyBook04.text19}>vocês</Text>
+              </View>
+              <View style={StyBook04.container11}/>
+              <View  style={StyBook04.container14}>
+                <Text style={StyBook04.text16}>have</Text>
+                <Text style={StyBook04.text17}>........</Text>
+                <Text style={StyBook04.text19}>tens</Text>
+                <Text style={StyBook04.text19}>tem</Text>
+                <Text style={StyBook04.text19}>têm</Text>
+              </View>
+            </View>
+            <View style={StyBook04.container12}/>
+            <View style={StyBook04.container7}>
+              <View  style={StyBook04.container13}>
+                <Text style={StyBook04.text16}>he / She</Text>
+                <Text style={StyBook04.text17}>....    ......</Text>
+                <Text style={StyBook04.text19}>ele / ela</Text>
+              </View>
+              <View style={StyBook04.container11}/>
+              <View  style={StyBook04.container13}>
+                <View style={StyBook04.container7}>
+                  <Text style={StyBook04.text16}>ha</Text>
+                  <Text style={StyBook04.text18}>s</Text>
+                </View>
+                <Text style={StyBook04.text17}>......</Text>
+                <Text style={StyBook04.text19}>tem</Text>
+              </View>
+            </View>
+            <View style={StyBook04.container12}/>
+            <View style={StyBook04.container7}>
+              <View  style={StyBook04.container13}>
+                <Text style={StyBook04.text16}>they</Text>
+                <Text style={StyBook04.text17}>......</Text>
+                <Text style={StyBook04.text19}>eles / elas</Text>
+              </View>
+              <View style={StyBook04.container15}/>
+              <View  style={StyBook04.container13}>
+                <Text style={StyBook04.text16}>have</Text>
+                <Text style={StyBook04.text17}>......</Text>
+                <Text style={StyBook04.text19}>têm</Text>
+              </View>
+            </View>
+          </View>
+          <View>
+            <View style={StyBook04.container7}>
+              <Text style={StyBook04.text20}>Note que os</Text>
+              <View>
+                <Text style={StyBook04.text11}> verbos </Text>
+                <Text style={StyBook04.text12}>...........</Text>
+              </View>
+              <Text style={StyBook04.text11}>em inglês mudam</Text>
+            </View>
+            <Text style={StyBook04.text13}>dependendo da pessoa, mas não tanto como</Text> 
+            <Text style={StyBook04.text13}>em português.</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
---
+++ screens/styless/styBook04.js
@@ -0,0 +1,251 @@
+import { StyleSheet } from "react-native";
+
+const StyBook04 = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    },
+    container2: {
+        marginTop: 10,
+        backgroundColor: '#808080',
+        width: '100%',
+        height: 3,
+    },
+    container3: {
+        flexDirection: 'row',
+        marginTop: 20,
+        marginLeft: 20,
+        width: 300,
+        height: 100,
+        borderColor: '#777',
+        borderRadius: 20,
+        borderWidth: 2,
+    },
+    container4: {
+        flexDirection: 'row',
+        marginTop: 20,
+        marginLeft: 20,
+        width: 400,
+        height: 100,
+        borderColor: '#777',
+        borderRadius: 20,
+        borderWidth: 2,
+    },
+    container5: {
+        flexDirection: 'row',
+        marginTop: 20,
+        marginLeft: 20,
+        width: 400,
+        height: 140,
+        borderColor: '#777',
+        borderRadius: 20,
+        borderWidth: 2,
+    },
+    container6: {
+        flexDirection: 'row',
+        marginTop: 20,
+        marginLeft: 20,
+        width: 340,
+        height: 100,
+        borderColor: '#777',
+        borderRadius: 20,
+        borderWidth: 2,
+    },
+    container7: {
+        flexDirection: 'row',
+    },
+    container8: {
+        backgroundColor: '#444',
+        marginTop: 20,
+        width: '100%',
+        height: 980,
+    },
+    container9: {
+        backgroundColor: '#222',
+        marginTop: 30,
+        marginLeft: 20,
+        width: '91%',
+        height: 497,
+        borderColor: '#87cefa',
+        borderRadius: 30,
+        borderWidth: 3,
+    },
+    container10: {
+        width: 190,
+        height: 50,
+    },
+    container11: {
+        backgroundColor: '#87cefa',
+        width: 3,
+        height: '100%',
+    },
+    container12: {
+        backgroundColor: '#87cefa',
+        width: '100%',
+        height: 3,
+    },
+    container13: {
+        width: 190,
+        height: 90,
+    },
+    container14: {
+        width: '49%',
+        height: 160,
+    },
+    container15: {
+        backgroundColor: '#87cefa',
+        width: 3,
+        height: 90,
+    },
+    container16: {
+        flexDirection: 'row',
+        marginTop: 20,
+        marginLeft: 20,
+        marginVertical: 10,
+        width: 270,
+        height: 100,
+        borderColor: '#777',
+        borderRadius: 20,
+        borderWidth: 2,
+    },
+
+    text1:{
+        marginLeft: 5,
+        marginTop: 10,
+        fontSize:25,
+        color: '#999'
+    },
+    text2:{
+        alignSelf: 'center',
+        fontSize:30,
+        color: '#ffffff'
+    },
+    text3:{
+        marginTop: 5,
+        alignSelf: 'center',
+        fontSize:24,
+        color: '#999'
+    },
+    text4:{
+        alignSelf: 'center',
+        fontSize:24,
+        color: '#999'
+    },
+    text5:{
+        marginTop: 20,
+        marginLeft: 20,
+        fontSize:18,
+        color: '#87cefa'
+    },
+    text6:{
+        marginTop: 25,
+        marginLeft:20,
+        fontSize:24,
+        color: '#ffffff'
+    },
+    text7:{
+        marginTop: 17,
+        marginLeft:10,
+        fontSize:20,
+        color: '#ffffff'
+    },
+    text8:{
+        marginTop: 10,
+        marginLeft:10,
+        fontSize:20,
+        color: '#999',
+    },
+    text9:{
+        marginTop: 25,
+        marginLeft:20,
+        fontSize:19,
+        color: '#999',
+    },
+    text10:{
+        marginLeft: 7,
+        marginTop: 25,
+        fontSize:19,
+        color: '#ffffff',
+    },
+    text11:{
+        marginTop: 25,
+        fontSize:19,
+        color: '#999',
+    },
+    text12:{
+        marginLeft: 6,
+        marginTop: -20,
+        fontSize: 19,
+        color: '#c0c0c0',
+    },
+    text13:{
+        marginTop: 5,
+        marginLeft:20,
+        fontSize:19,
+        color: '#999',
+    },
+    text14:{
+        marginTop: 5,
+        fontSize:19,
+        color: '#999',
+    },
+    text15:{
+        marginLeft: 5,
+        marginTop: 5,
+        fontSize:19,
+        color: '#fff',
+    },
+    text16:{
+        marginLeft: 20,
+        marginTop: 10,
+        fontSize:19,
+        color: '#ffffff',
+    },
+    text17:{
+        marginLeft: 18,
+        marginTop: -20,
+        fontSize: 19,
+        color: '#c0c0c0',
+    },
+    text18:{
+        marginTop: 10,
+        fontSize:19,
+        color: '#87cefa',
+    },
+    text19:{
+        marginLeft: 20,
+        marginTop: 10,
+        fontSize:19,
+        color: '#888',
+    },
+    text20:{
+        marginTop: 25,
+        marginLeft:20,
+        fontSize:19,
+        color: '#999',
+    },
+
+    scroll: {
+        backgroundColor: '#222',
+        width: '100%',
+        height: '100%',
+    },
+
+    imag01: {
+        marginLeft: 15,
+        alignSelf: 'center',
+        height: 180,
+        width: 180
+    },
+
+    icon1: {
+        color: '#87cefa',
+        fontSize: 30,
+        marginTop: 30,
+        marginLeft: 10,
+    },
+       
+});
+  export default StyBook04
