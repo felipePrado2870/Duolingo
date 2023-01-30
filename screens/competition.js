9c32e3796b003dd752b36d3ff04b110dc6d87334
Felipe do Prado <felippe_praddo287@hotmail.com>
2023-01-29 21:49:17 -0300

    adjusted competition screens layout
    adjusted competition screens layout

--- screens/competition.js
+++ screens/competition.js
@@ -1,23 +1,56 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { Image, Text, View} from 'react-native';
+import StyCompetition from './styless/styCompetition';
+const Imagem01 = require('../assets/competition/Image01.jpg')
+const Imagem02 = require('../assets/competition/Image02.jpg')
+const Imagem03 = require('../assets/competition/Image03.jpg')
+const Icon1 = require('../assets/competition/first-place.png')
+const Icon2 = require('../assets/competition/second-place.png')
+const Icon3 = require('../assets/competition/third-place.png')
 
 function ScreenCompetition({navigation}) {
   
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#000000',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text1:{
-      fontSize:15,
-      color: '#fff'
-    }
-  });
   return (
-    <View style={styles.container}>
-      <Text style={styles.text1}> ScreenCompetition</Text>
+    <View style={StyCompetition.container}>
+      <View style={StyCompetition.container1}>
+        <View style={StyCompetition.container2}>
+          <Image style={StyCompetition. containerImgeg02} source={Imagem02} resizeMode= "stretch"/>
+        </View>
+        <View style={StyCompetition.container3}>
+          <Image style={StyCompetition. containerImgeg01} source={Imagem01} resizeMode= "stretch"/>
+        </View>
+        <View style={StyCompetition.container4}>
+          <Image style={StyCompetition. containerImgeg03} source={Imagem03} resizeMode= "stretch"/>
+        </View>
+      </View> 
+      <View style={StyCompetition.container5}>
+        <Image style={StyCompetition. containerIcon02} source={Icon2} resizeMode= "stretch"/>
+        <Image style={StyCompetition. containerIcon01} source={Icon1} resizeMode= "stretch"/>
+        <Image style={StyCompetition. containerIcon03} source={Icon3} resizeMode= "stretch"/>
+      </View>
+      <View style={StyCompetition.container5}>
+        <View style={StyCompetition.containerText02}>
+          <Text style={StyCompetition.text1}>sofia</Text>
+          <Text style={StyCompetition.text1}>531 XP</Text>
+        </View>
+        <View style={StyCompetition.containerText01}>
+          <Text style={StyCompetition.text1}>Ash</Text>
+          <Text style={StyCompetition.text1}>2546 XP</Text>
+        </View>
+        <View style={StyCompetition.containerText03}>
+          <Text style={StyCompetition.text1}>Orli Chag..</Text>
+          <Text style={StyCompetition.text1}>476 XP</Text>
+        </View>
+      </View>
+      <View style={StyCompetition.container6}>
+        <Text style={StyCompetition.text2}>Já temos os Resultados!</Text>
+        <Text style={StyCompetition.text3}>Aqui estão os vencedores da</Text>
+        <Text style={StyCompetition.text4}>última semana na Divisão</Text>
+        <Text style={StyCompetition.text4}>Bronze.</Text>
+      </View>
+      <View style={StyCompetition.container7}>
+        <Text style={StyCompetition.text5}>CONTINUAR</Text>
+      </View>
     </View>
   );
 }
---
+++ screens/styless/styCompetition.js
@@ -0,0 +1,144 @@
+import { StyleSheet } from "react-native";
+
+const StyCompetition = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: '#000000',
+        alignItems: 'center',
+    },
+    container1: {
+        flexDirection: 'row',
+        marginTop: 200,
+    },
+    container2: {
+        height: 100,
+        width: 100,
+        marginTop: 20,
+        borderRadius: 50,
+        borderColor: '#b0e0e6',
+        borderWidth: 3,
+        alignItems: 'center',
+        justifyContent:'center',
+    },
+    container3: {
+        height: 120,
+        width: 120,
+        marginLeft: 20,
+        borderRadius: 80,
+        borderColor: '#ffd700',
+        borderWidth: 3,
+        alignItems: 'center',
+        justifyContent:'center',
+    },
+    container4: {
+        height: 90,
+        width: 90,
+        marginTop: 30,
+        marginLeft: 20,
+        borderRadius: 50,
+        borderColor: '#cd853f',
+        borderWidth: 3,
+        alignItems: 'center',
+        justifyContent:'center',
+    },
+    container5: {
+        flexDirection: 'row',
+        width: '100%',
+    },
+    container6:{
+        width: '100%',
+        alignItems: 'center',
+        marginTop: 50,
+    },
+    container7:{
+        width: 390,
+        height: 45,
+        marginTop: 100,
+        backgroundColor: '#87ceeb',
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+  
+    containerImgeg01:{
+        height: 105,
+        width: 105,
+        backgroundColor: '#222',
+        borderRadius: 80,
+        borderWidth: 2,
+    },
+    containerImgeg02:{
+        height: 85,
+        width: 85,
+        backgroundColor: '#222',
+        borderRadius: 50,
+        borderWidth: 2,
+    },
+    containerImgeg03:{
+        height: 75,
+        width: 75,
+        backgroundColor: '#222',
+        borderRadius: 50,
+        borderWidth: 2,
+    },
+  
+    containerIcon01: {
+        height: 50,
+        width: 50,
+        marginTop: -50,
+        marginLeft: 85,
+    },
+    containerIcon02:{
+        height: 50,
+        width: 50,
+        marginTop: -50,
+        marginLeft: 110,
+    },
+    containerIcon03: {
+        height: 50,
+        width: 50,
+        marginTop: -50,
+        marginLeft: 65,
+    },
+  
+    containerText01:{
+        height: 40,
+        width: 120,
+        marginLeft: 15,
+    },
+    containerText02:{
+        height: 40,
+        width: 120,
+        marginLeft: 30,
+    },
+    containerText03:{
+        height:40,
+        width: 120,
+        marginLeft: 5,
+    },
+     
+    text1:{
+        alignSelf:'center',
+        fontSize:20,
+        color: '#444'
+    },
+    text2:{
+        fontSize:35,
+        color: '#fff'
+    },
+    text3:{
+        fontSize:25,
+        marginTop: 15,
+        color: '#c0c0c0'
+    },
+    text4:{
+        fontSize:25,
+        color: '#c0c0c0'
+    },
+    text5:{
+        fontSize:25,
+        color: '#000',
+    },
+});
+  export default StyCompetition
