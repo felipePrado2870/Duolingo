8c44474541022751839adad7c22d1e5e1bed321c
Felipe do Prado <felippe_praddo287@hotmail.com>
2023-01-31 10:24:41 -0300

    added styChallenges on challenges screen
    added styChallenges on challenges screen

---
+++ screens/styless/styChallenges.js
@@ -0,0 +1,257 @@
+import { StyleSheet } from "react-native";
+
+const StyChallenges = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: '#000000',
+        alignItems: 'center',
+      },
+      container1: {
+        flex: 1,
+        width: '100%',
+        marginTop : 20,
+      },
+      container2: {
+        marginHorizontal: 20,
+        marginTop : 20,
+        height: 350,
+        width: 390,
+        borderRadius: 20,
+        borderWidth: 2,
+        borderColor: '#7777'
+      },
+      container3: {
+        height: 90,
+        width: 390,
+        flexDirection: 'row',
+      },
+      container4: {
+        height: 120,
+        width: 390,
+        flexDirection: 'row',
+      },
+      container5: {
+        marginHorizontal: 20,
+        marginVertical: 30,
+        marginTop : 20,
+        height: 370,
+        width: 390,
+        borderRadius: 20,
+        borderWidth: 2,
+        borderColor: '#7777',
+      },
+      container6: {
+        height: 200,
+        width: 200,
+        marginLeft: 15,
+      },
+      container7: {
+        marginTop: 15,
+        height: 35,
+        width: 115,
+        backgroundColor: '#ffff',
+        borderRadius: 4,
+        marginVertical: 10,
+      },
+      container8: {
+        marginLeft: 15,
+        height: 40,
+        width: 250,
+      },
+  
+      scroll: {
+        width: '100%',
+      },
+  
+      text:{
+        marginTop: 20,
+        fontSize:25,
+        color: '#666'
+      },
+      text3 :{
+        fontSize:25,
+        color: '#ffff',
+      },
+      text4 :{
+        marginLeft: 10,
+        fontSize:20,
+        color: '#daa520',
+        alignSelf: 'center',
+      },
+      text5 :{
+        fontSize:25,
+        color: '#ffff',
+      },
+      text6 :{
+        marginLeft: 5,
+        fontSize:20,
+        color: '#daa520',
+      },
+      text7 :{
+        marginLeft: 5,
+        fontSize:25,
+        color: '#4169e1',
+      },
+      text8 :{
+        fontSize:25,
+        color: '#ffff'
+      },
+      text9 :{
+        marginTop: 5,
+        fontSize:20,
+        color: '#c0c0c0',
+      }, 
+      text10 :{
+        marginTop: 5,
+        fontSize:21,
+        color: '#ffff',
+      }, 
+      text11 :{
+        fontSize:20,
+        color: '#c0c0c0',
+      }, 
+      text12 :{
+        marginTop: 5,
+        fontSize:20,
+        color: '#1e90ff',
+      }, 
+      text13 :{
+        marginTop: 7,
+        marginLeft: 15,
+        fontSize:22,
+        color: '#ffff'
+      },
+      text14 :{
+        fontSize:35,
+        color: '#ffff',
+        alignSelf: 'center',
+      },
+      text15 :{
+        marginTop: 30,
+        fontSize:24,
+        color: '#c0c0c0',
+        alignSelf: 'center',
+      },
+      text16 :{
+        fontSize:24,
+        color: '#c0c0c0',
+        alignSelf: 'center',
+      },
+      
+      view1:{
+        flexDirection: 'row',
+        marginTop: 30
+      },
+      view2:{
+        flex: 1,
+      },
+      view3:{
+        marginLeft: 20,
+        height: 40,
+        width:'100%',
+        flexDirection: 'row', 
+      },
+      view4:{
+        height: 40,
+        width: 290,
+      },
+      view5:{
+        height: 40,
+        width: 150,
+        flexDirection: 'row', 
+      },
+      view6:{
+        marginTop: 5,
+        height: 2,
+        backgroundColor: '#7777',
+      },
+      view7:{
+        marginTop: 35,
+        marginLeft: 15,
+        marginHorizontal: 5,
+        height: 50,
+        width: 50,
+        backgroundColor: '#1e90ff',
+        borderRadius: 20,
+      },
+      view8:{
+        marginTop: 10,
+        marginLeft: 15,
+        height: 17,
+        width: 200,
+        backgroundColor: '#7777',
+        borderRadius: 10,
+      },
+      view9:{
+        marginTop: 10,
+        marginLeft: 15,
+        height: 30,
+        width: 250,
+      },
+      view10:{
+        marginTop: 10,
+        marginLeft: 15,
+        height: 65,
+        width: 300,
+      },
+      view11:{
+        flexDirection: 'row', 
+      },
+      view12:{
+        marginTop: 30,
+        marginLeft: 15,
+        height: 17,
+        width: 340,
+        backgroundColor: '#7777',
+        borderRadius: 10,
+      },
+      
+      
+      imagem01: {
+        alignSelf: 'center',
+        height: 25,
+        width: 25,
+      },
+      imagem02: {
+        marginLeft: 10,
+        height: 60,
+        width: 60,
+        alignSelf: 'center',
+      },
+      imagem03: {
+        height: 50,
+        width: 50,
+        alignSelf: 'center',
+      },
+      imagem04: {
+        height: 60,
+        width:60,
+        marginLeft:-30,
+        marginTop:-10,
+      },
+      imagem05: {
+        height: 150,
+        width:150,
+        marginTop: 40,
+        marginLeft:10,
+      },
+      imagem06: {
+        height: 50,
+        width:50,
+        marginLeft:-30,
+        marginTop: 12,
+        borderRadius: 50,
+        borderWidth: 3,
+        borderColor: '#000000',
+      },
+      imagem07: {
+        marginTop: 60,
+        alignSelf: 'center',
+        height: 180,
+        width: 230,
+        marginVertical: 10,
+      },
+  
+});
+  export default StyChallenges
